52650a83733a1ce8d3e19377927a29bc56607c05
jsieving <jane.sieving@students.olin.edu>
2018-03-30 15:40:33 -0400

    made scheduling compatible with datetime

--- Scheduling/agenda_gen.py
+++ Scheduling/agenda_gen.py
@@ -2,6 +2,7 @@
 Provides helper functions for the scheduling algorithm, particularly to provide dummy data. '''
 
 from random import sample, randint, choice
+from datetime import timedelta, datetime, date, time
 
 class Item:
     '''An event created with or without scheduling information.
@@ -24,9 +25,22 @@ class Item:
         self.category = category # may be replaced with tags?
 
     def __str__(self):
-        return "\t%f | %s from %s to %s" % (self.preference, self.name, self.start, self.end)
+        return "\t%f | %s from %s to %s" % (self.preference, self.name, self.start.time(), self.end.time())
 
 categories = ['QEA', 'softdes', 'eating', 'nap']
+
+def min_to_dt(minutes, d = date(1, 1, 1)):
+    hours, minutes = divmod(minutes, 60)
+    t = time(hours, minutes)
+    dt = datetime.combine(d, t)
+    return dt
+
+def min_to_timedelta(minutes):
+    hours, minutes = divmod(minutes, 60)
+    return timedelta(hours = hours, minutes = minutes)
+
+def min_from_dt(dt):
+    return dt.hour * 60 + dt.minute
 
 def random_events(minimum, maximum, n):
     '''Returns a list of n randomly created events with duration between the min and max,
@@ -35,7 +49,7 @@ def random_events(minimum, maximum, n):
     for i in range(n):
         start = None
         end = None
-        duration = randint(minimum, maximum)
+        duration = min_to_timedelta(randint(minimum, maximum))
         importance = randint(1, 5)
         category = choice(categories)
         name = category + ' ' + str(randint(0, 10))
@@ -46,22 +60,23 @@ def random_events(minimum, maximum, n):
 def random_timeblocks(n, blocklength = 15):
     '''Returns a list of n non-overlapping timeblocks, which are (start, end) tuples
     spanning a whole number of blocklengths. '''
-    free_time = []
+    free_times = []
     blocks = range(0, 1440//blocklength)
     times = sample(blocks, 2*n)
     times.sort()
     for i in range(n):
-        time_span = times.pop(0) * 15, times.pop(0) * 15
-        free_time.append(time_span)
-    return free_time
+        start, end = times.pop(0) * blocklength, times.pop(0) * blocklength
+        time_span = min_to_dt(start), min_to_dt(end)
+        free_times.append(time_span)
+    return free_times
 
-def busy_to_free(busy_time):
+def busy_to_free(busy_times):
     '''Takes a list of timeblocks in a day and returns the list timeblocks that are not
     covered, for example given a list of busy times it will return all unused times. '''
-    day = (0, 1440)
-    free_time = []
-    for start, end in busy_time:
+    day = (min_to_dt(0), min_to_dt(1439))
+    free_times = []
+    for start, end in busy_times:
         time_span = day[0], start
         day = end, day[1]
-        free_time.append(time_span)
-    return free_time
+        free_times.append(time_span)
+    return free_times
--- Scheduling/scheduling.py
+++ Scheduling/scheduling.py
@@ -2,6 +2,7 @@
 Schedules the contents of a todo list into a calendar based on user's free time and other preferences. '''
 
 from agenda_gen import *
+from datetime import timedelta, datetime, date, time
 
 class Item:
     '''An event created with or without scheduling information.
@@ -24,7 +25,7 @@ class Item:
         self.category = category # may be replaced with tags?
 
     def __str__(self):
-        return "\t%f | %s from %s to %s" % (self.preference, self.name, self.start, self.end)
+        return "\t%f | %s from %s to %s" % (self.preference, self.name, self.start.time(), self.end.time())
 
 class Calendar:
     def __init__(self, name):
@@ -35,7 +36,7 @@ class Day:
     def __init__(self, date):
         self.date = date
         self.events = []
-        self.free_time = [(0, 600), (700, 1400)]
+        self.free_times = [(min_to_dt(0), min_to_dt(1439))]
 
     def print_events(self):
         print('Today is %s. You have %i events.' % (self.date, len(self.events)))
@@ -45,18 +46,18 @@ class Day:
 def schedule_day(day, task_list):
     '''Given a day and a list of tasks, schedules as many of the events as will fit
     in the free time that day. '''
-    free_time = day.free_time # List of available time slots in order of occurence
-    while len(free_time) > 0 and len(todos) > 0: # While there's stuff to do and time to do it...
-        time_slot = free_time[0]
+    free_times = day.free_times # List of available time slots in order of occurence
+    while len(free_times) > 0 and len(todos) > 0: # While there's stuff to do and time to do it...
+        time_slot = free_times[0]
         avail_time = time_slot[1]-time_slot[0] # Duration of the first available time slot
         fit_tasks = []
         for task in task_list: # Make a list of tasks that could be completed during this time
             if task.duration <= avail_time:
                 fit_tasks.append(task)
         if len(fit_tasks) == 0: # If no tasks fit in this block, delete it from the queue and skip it
-            del free_time[0]
+            del free_times[0]
             continue
-        time_area = time_slot[0]//480 # What part of the day it is, to determine user's work time preferences
+        time_area = min_from_dt(time_slot[0])//480 # What part of the day it is, to determine user's work time preferences
         for task in fit_tasks: # Preference is a function of importance and other variables
             task.preference = task.importance  + effectiveness[task.category][time_area][0]# - hours_til_due/48 or something
         fit_tasks.sort(key=lambda r:r.preference) # sort tasks by preference
@@ -64,28 +65,28 @@ def schedule_day(day, task_list):
         fit_tasks[0].end = time_slot[0] + fit_tasks[0].duration # Assign end time
 
         if fit_tasks[0].duration + min_event_time >= avail_time: # If a task will consume a time block, delete it entirely
-            del free_time[0]
+            del free_times[0]
         else:
-            free_time[0] = (fit_tasks[0].end, time_slot[1]) # If there will be some time left over, make that the next time block
+            free_times[0] = (fit_tasks[0].end, time_slot[1]) # If there will be some time left over, make that the next time block
         day.events.append(fit_tasks[0]) # Since the task fits and is scheduled, add it to the day's events
         task_list.remove(fit_tasks[0]) # Remove it from the list of things to be scheduled
 
 def add_item(name, duration, importance, category):
     '''Creates a basic unscheduled item and adds it to the todo list'''
-    new_item = Item(name, duration = duration, importance = importance, category = category)
+    new_item = Item(name, duration = timedelta(minutes = duration), importance = importance, category = category)
     todos.append(new_item)
 
-date = '08/09/2004'
 calendar = Calendar('My Calendar')
-today = Day(date) # In practice, these would be initialized and stored in the calendar long ahead of time
-calendar.days[date] = today
+t = date(2004, 8, 9)
+today = Day(t) # In practice, these would be initialized and stored in the calendar long ahead of time
+calendar.days[t] = today
 
-busy_time = random_timeblocks(8) # In practice, this would come from the events scheduled today
-print(busy_time)
-today.free_time = busy_to_free(busy_time)
-print(today.free_time)
+busy_times = random_timeblocks(8) # In practice, this would come from the events scheduled today
+# print(busy_times)
+today.free_times = busy_to_free(busy_times)
+# print(today.free_times)
 
-min_event_time = 20 # This would be a user preference
+min_event_time = timedelta(minutes = 20) # This would be a user preference
 todos = random_events(20, 120, 4) # Make a random to-do list for testing
 '''
 effectiveness: describes effectiveness/preference of working on 'task_type' during 'time_area'
@@ -96,5 +97,5 @@ effectiveness = {'QEA': [(.5, 1), (0, 1), (-.5, 1)], 'softdes': [(-.5, 1), (0, 1
                 'eating': [(-.5, 1), (.5, 1), (-.5, 1)], 'nap': [(.5, 1), (0, 1), (.5, 1)]}
 
 
-schedule_day(today, todos)
+schedule_day(today, todos) # repeated event needs to be passed as a string composed of params
 today.print_events()
