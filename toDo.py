f3c05099e7d81568c2818d71b09b9ac4b1616821
vscheyer <vscheyer@comcast.net>
2018-04-14 17:21:47 -0400

    Working on todo.py

--- toDo.py
+++ toDo.py
@@ -5,16 +5,6 @@ from pickle import dump, load
 Adds event object to a list and adds list to file
 """
 
-def make_todo(todo_list)
-    my_todo = []
-    fileObject = open(file_name, 'wb')
-    pickle.dump(fileObject, my_todo)
-
-    todo = open('todo.txt', 'w+')
-    todo.write(str(my_todo)) # write to do list to file
-
-    def event_to_list(event):
-        my_todo.append(event)
-        todo = open('todo.txt', 'w+')
-        todo.write(str(my_todo)) # write to do list to file
-        return todo
+def make_list(to_do):
+    fileObject = open(testData/toDo, 'wb')
+    pickle.dump(fileObject, to_do)
