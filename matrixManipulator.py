71283a38a98c0fcb6402ec3e6815c088ba5beb88
wFairmanOlin <will@students.olin.edu>
2018-04-17 09:50:31 -0400

    added support for timeslots that do not divde into the day equally

--- matrixManipulator.py
+++ matrixManipulator.py
@@ -3,13 +3,11 @@ import random
 from scheduleHelpers import Item, Calendar
 from pickle import load, dump
 
-def getCostMatrix(block):
+def getCostMatrix(length):
     matrix = []
-    day = 86400
-    numSegments = int(day/block)
-    for column in range(numSegments):
+    for column in range(length):
         matrix.append([])
-        for row in range(numSegments):
+        for row in range(length):
             matrix[column].append('N/A')
     return matrix
 
@@ -20,7 +18,7 @@ def getTaskList(timeSlots, itemList):
     for i in range(len(itemList)):
         itemDuration.append(itemList[i].duration.total_seconds())
     for i in range(len(timeSlots)):
-        if itemDuration[i] != timeBlock:
+        if itemDuration[i] != timeBlock or i > len(itemList):
             retList.append('break')
         else:
             retList.append(itemList[i])
@@ -30,7 +28,9 @@ def getTaskList(timeSlots, itemList):
 def getListofTime(block):
     day = 86400
     if day % block != 0:
-        raise ValueError('Please enter a time that can divide into {} evenly'.format(day))
+        divisions = int(day/block)
+        return numpy.linspace(0, block*(divisions), divisions+1)
+        #raise ValueError('Please enter a time that can divide into {} evenly'.format(day))
     else:
         return numpy.linspace(0, day-block, day/block)
 
@@ -48,11 +48,21 @@ def getItemList(cal):
             itemList.append(event)
     return itemList
 
+def populateMatrix(timeList, taskList, matrix):
+    for y in range(len(taskList)):
+        for x in range(len(timeList)):
+            if x == y:
+                matrix[x][y] = 1
+            else:
+                matrix[x][y] = 0
+    return matrix
+
 def main(itemList):
     timeBlock = getLongestBlock(itemList)
     timeList = getListofTime(timeBlock)
     taskList = getTaskList(timeList, testList)
-    costMatrix = getCostMatrix(timeBlock)
+    costMatrix = getCostMatrix(len(timeList))
+    costMatrix = populateMatrix(timeList, taskList, costMatrix)
     print('list of time slots:', timeList)
     print('list of events:', taskList)
     print('cost matrix:', costMatrix)
@@ -62,4 +72,7 @@ if __name__ == "__main__":
     testCal = load(tempFile)
     testList = getItemList(testCal)
     del(testList[8])
+    #print(getListofTime(4000))
+    # for i in testList:
+        # print(i)
     main(testList)
