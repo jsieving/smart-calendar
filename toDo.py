761fc8fc3f45b8417d27854fd5b7aee1b0bfb28c
vscheyer <vscheyer@comcast.net>
2018-04-27 10:36:42 -0400

    Added code for appending calendar events to todo list (commented out until we have calendar list

--- toDo.py
+++ toDo.py
@@ -5,15 +5,16 @@ from scheduleHelpers import Item
 import os
 from gcal import GCal
 
-""" Import this .py into another .py, call the event_to_list function
-Adds event object to a list and adds list to file
+""" Call the make_list function to add an event object to a list and adds list to file
+It also updates the gcal list every time it gets run
 """
-
+# # for setup and testing purposes
 # my_event = Item('hw')
 # foo = []
 # dump(foo, open("testData/listData", "wb"))
 # print("tested")
 
+# def make_list(item, cal_list)
 def make_list(item):
     try:
         f = open("testData/listData", "rb+")
@@ -29,5 +30,15 @@ def make_list(item):
     dump(todo_list, f)
     f.close()
     print("added to file")
+
+    # # check for duplicate calendar events in saved list
+    # # add new calendar events to todo_list
+    # for i in range(0,len(cal_list)):
+    #     add = True
+    #     for j in range(0,len(todo_list)):
+    #         if cal_list[i] == todo_list[j]:
+    #             add = False
+    #     if add == True:
+    #         todo_list.append(cal_list[i])
 
     f.close()
