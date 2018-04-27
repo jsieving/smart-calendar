9f2b244deea5404ab27df9d056696b5d3bf4bc73
wFairmanOlin <will@students.olin.edu>
2018-04-27 10:47:49 -0400

    added a way to get a list of events from gcal

--- saveToCal.py
+++ saveToCal.py
@@ -1,13 +1,35 @@
 from datetime import datetime
 from gcal import GCal
 
-def main():
-    cal = GCal
-    date = datetime.now()
-    print(date.day)
-    listOfEvents = cal.get_events()
-    print(listOfEvents)
+def getListOfEvents(tempList):
+    """
+    This function returns a list of events that are not repeatable
+    """
+    retList = []
+    for i in tempDict:
+        if 'recurrence' not in i.keys():
+            retList.append(i)
+    return retList
 
+def main():
+    cal = GCal()
+
+    events = cal.get_events()
+    listOfItems = events['items']
+
+    print(getListOfEventspr
+    print(listOfItems[0])
 
 if __name__ == "__main__":
     main()
+
+'''
+self, name, start = None, end = None, duration = None,\
+            breakable = False, importance = None, category = None, item_type = 'event'):
+
+This file stores on-demand the events that are in google calendar into a person's history.
+
+This file should only store events that were created by the user or the alogrithm.
+
+If the function tries to save the same event, it should spit an error
+'''
